39f1e31457b4a74fb63dd61be79ad1b85f91c2b0
Thomas Cross <tom.bz2@gmail.com>
2022-10-29 14:37:15 -0500

    add strike_api_key

--- strike_api/base.py
+++ strike_api/base.py
@@ -2,15 +2,15 @@ import os
 import requests
 
 def call_api(method, url, headers=None, data=None):
-    token = os.environ.get("TOKEN")
-    if not token:
-        raise EnvironmentError("TOKEN not found in environment variables")
+    strike_api_key = os.environ.get("STRIKE_API_KEY")
+    if not strike_api_key:
+        raise EnvironmentError("STRIKE_API_KEY not found in environment variables")
 
     if not headers:
         headers = {}
 
     headers["Accept"] = "application/json"
-    headers["Authorization"] = f"Bearer {token}"
+    headers["Authorization"] = f"Bearer {strike_api_key}"
 
     response = requests.request(method, url, headers=headers, data=data)
 
