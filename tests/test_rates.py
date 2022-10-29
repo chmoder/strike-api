1779112580aed880896452415dbd16605aede9de
Thomas Cross <tom.bz2@gmail.com>
2022-10-29 14:40:44 -0500

    fix test

--- tests/test_rates.py
+++ tests/test_rates.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 from strike_api.rates import get_ticker
 
 def test_get_ticker():
-    assert isinstance(get_ticker(), dict)
+    assert isinstance(get_ticker(), list)
