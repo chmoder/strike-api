ddeea8b19f7c415c548b9243ce28968cb7a7bce5
Thomas Cross <tom.bz2@gmail.com>
2022-11-04 12:47:05 -0500

    add invoices params

--- strike_api/invoices.py
+++ strike_api/invoices.py
@@ -5,14 +5,14 @@ from strike_api.base import call_api
 
 
 def get_invoices(
-    filter: str = None, orderby: str = None, skip: int = None, top: int = None
+    filter_: str = None, orderby: str = None, skip: int = None, top: int = None
 ) -> typing.List[dict]:
     """Get Invoices
     Required scopes: partner.webhooks.manage
     OData filtering syntax can be seen `here <https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-odata/7d6c4117-317d-4860-915b-7e321be017e3>`_.  Ordering syntax can be seen `here <https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-odata/793b1e83-95ee-4446-8434-f5b634f20d1e>`_.
 
     Args:
-        filter (str, optional): Filter the results using OData syntax. Supported properties: invoiceId, created, currency, state, issuerId, receiverId, payerId, correlationId. Defaults to None.
+        filter_ (str, optional): Filter the results using OData syntax. Supported properties: invoiceId, created, currency, state, issuerId, receiverId, payerId, correlationId. Defaults to None.
         orderby (str, optional): Order the results using OData syntax. Supported properties: created. Defaults to None.
         skip (int, optional): Skip the specified number of entries. Defaults to None.
         top (int, optional): Get the top X number of records. Default value: 50. Max value: 100. Defaults to None.
@@ -22,9 +22,9 @@ def get_invoices(
     """
     url = "https://api.strike.me/v1/invoices"
 
-    payload = {}
+    payload = {"filter": filter_, "orderby": orderby, "skip": skip, "top": top}
 
-    return call_api("GET", url, data=payload)
+    return call_api("GET", url, params=payload)
 
 
 def get_invoice(invoice_id: str) -> dict:
