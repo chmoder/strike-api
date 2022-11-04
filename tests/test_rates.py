bcde904706ba4fed069869ff0216c2c0a4f30464
Thomas Cross <tom.bz2@gmail.com>
2022-11-03 19:50:21 -0500

    add vcr to rates tests

--- tests/test_rates.py
+++ tests/test_rates.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
+import pytest
 from strike_api.rates import get_ticker
 
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {"filter_headers": ["Authorization"]}
 
+@pytest.mark.vcr
 def test_get_ticker():
     assert isinstance(get_ticker(), list)
