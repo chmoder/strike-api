28eb8e5d56420ec866d6076dbddfc16e72ac4a19
Thomas Cross <tom.bz2@gmail.com>
2022-11-05 10:15:29 -0500

    convert model members to snake_case

--- strike_api/models/generic.py
+++ strike_api/models/generic.py
@@ -1,15 +1,7 @@
 from __future__ import annotations
 import humps
-import re
 
 from pydantic import BaseModel
-
-
-def to_snake_case(name: str) -> str:
-    name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
-    name = re.sub("__([A-Z])", r"_\1", name)
-    name = re.sub("([a-z0-9])([A-Z])", r"\1_\2", name)
-    return name.lower()
 
 
 class StrikeAPIModel(BaseModel):
